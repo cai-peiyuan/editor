52232546dc61aa0eabdc513c4cabdc5de4299f91
peiyuan.cai-office <peiyuan.cai@mapabc.com>
2021-01-26 16:53:43 +0800

    Update lang.js
    读取系统全局语言变量

--- src/libs/lang.js
+++ src/libs/lang.js
@@ -1,4 +1,3 @@
-let lang = "zh";
 import langDict from '../config/lang.json'
 
 
@@ -8,6 +7,7 @@ import langDict from '../config/lang.json'
  * @returns {*}
  */
 export function getLableName(lableCode) {
+  let lang = window.lang || "zh";
   if(langDict[lableCode] && langDict[lableCode][lang]){
     return langDict[lableCode][lang];
   }else{
