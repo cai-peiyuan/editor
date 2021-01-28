f21d57b17a6f2f661db00b49d2927f8572f1c793
peiyuan.cai-office <peiyuan.cai@mapabc.com>
2021-01-28 20:44:39 +0800

    Update layer.js

--- src/libs/layer.js
+++ src/libs/layer.js
@@ -1,6 +1,7 @@
 import {latest} from '@mapbox/mapbox-gl-style-spec'
 import styleLayerDic from '../config/layer-dic.json'
 import sourceLayerDic from '../config/source-layer.json'
+import {getToken} from "../util/auth";
 
 export function changeType(layer, newType) {
   const changedPaintProps = { ...layer.paint }
@@ -87,6 +88,62 @@ export function getSourceLayerChnNameById(id) {
   return runConfig.sourceLayerLang && runConfig.sourceLayerLang =='en' ? id : (sourceLayerDic[id] || id);
 }
 
+/**
+ * 将数据源图层信息添加至msp
+ * @param layerSource
+ * @param layerId
+ */
+export function addSourceLayerToMsp(layerSource, layerId){
+//  {"id":null,"layerType":"source","layerGroup":null,"layerId":"1","layerName":"1","layerComment":"1"}
+
+    fetch(api_config.url + '/api/mapStyleEditorLayer', {
+      method: "POST",
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        'Authorization': getToken(),
+      },
+      body: JSON.stringify({"id":null,"layerType":"source","layerGroup":layerSource,"layerId":layerId,"layerName":sourceLayerDic[layerId],"layerComment":sourceLayerDic[layerId]})
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then((body) => {
+        console.log(body)
+      })
+      .catch(function(error) {
+        if(error) console.error(error)
+      })
+}
+
+
+/**
+ * 将样式图层信息添加至msp
+ * @param layerGroup
+ * @param layerId
+ */
+export function addStyleLayerToMsp(layerGroup, layerId, layerName, layerComment){
+//  {"id":null,"layerType":"source","layerGroup":null,"layerId":"1","layerName":"1","layerComment":"1"}
+
+  fetch(api_config.url + '/api/mapStyleEditorLayer', {
+    method: "POST",
+    mode: 'cors',
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      'Authorization': getToken(),
+    },
+    body: JSON.stringify({"id":null,"layerType":"style","layerGroup":layerGroup,"layerId":layerId,"layerName":layerName,"layerComment":layerComment})
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then((body) => {
+      console.log(body)
+    })
+    .catch(function(error) {
+      if(error) console.error(error)
+    })
+}
 
 /**
  * 根据图层样式文件分析图层字典表
@@ -102,18 +159,24 @@ export function getLayerChnNameDicByStyleFile(mapStyle) {
   console.log("分析字典表。。。。")
   mapStyle.layers.forEach((layer, index) => {
     if(layer["source-layer"]){
+      // addSourceLayerToMsp(layer["source"], layer["source-layer"])
       sourceLayerDic[layer["source-layer"]] = layer["source-layer"];
     }
     if(layer.metadata){
       if(layer.metadata.type_level1_chn && layer.metadata.type_level1_eng){
         layerDic[layer.metadata.type_level1_eng] = layer.metadata.type_level1_chn
+        // addStyleLayerToMsp('type_level1_eng', layer.metadata.type_level1_eng, layer.metadata.type_level1_chn ,'type_level1_eng' );
       }
       if(layer.metadata.type_level2_chn && layer.metadata.type_level2_eng){
         layerDic[layer.metadata.type_level2_eng] = layer.metadata.type_level2_chn
         layerDic[layer.id] = layer.metadata.type_level2_chn
+        // addStyleLayerToMsp('type_level2_eng', layer.type_level2_eng, layer.metadata.type_level2_chn ,'type_level2_eng' );
+        // addStyleLayerToMsp(layer.metadata.type_level2_chn, layer.id, layer.metadata.type_level2_chn || layer.metadata["maputnik:comment"]  ,layer.metadata["maputnik:comment"] || layer.metadata.type_level2_chn );
       }
       if(layer.metadata.type_level3_chn && layer.metadata.type_level3_eng){
         layerDic[layer.metadata.type_level3_eng] = layer.metadata.type_level3_chn
+
+        // addStyleLayerToMsp('type_level3_eng', layer.type_level3_eng, layer.metadata.type_level3_chn ,'type_level3_eng' );
       }
       if(layer.metadata["maputnik:comment"]){
        // layerDic[layer.id] = layer.metadata["maputnik:comment"]
